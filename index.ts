351443f3e182e75d6b6df8c371fea0da3c006846
teramotodaiki <i101323@gmail.com>
2024-05-04 19:08:49 +0900

    /bot endpointを追加

--- index.ts
+++ index.ts
@@ -6,49 +6,51 @@ type Bindings = {
   OPENAI_API_KEY: string;
 };
 
-const app = new Hono<{ Bindings: Bindings }>().post("/", async (c) => {
-  const command = await c.req.formData();
-  console.log(command);
+const app = new Hono<{ Bindings: Bindings }>()
+  .post("/command", async (c) => {
+    const command = await c.req.formData();
+    console.log(command);
 
-  request(c.env, command.get("response_url") + "", "POST", {
-    text: "考えています…",
-  });
+    request(c.env, command.get("response_url") + "", "POST", {
+      text: "考えています…",
+    });
 
-  const input = command.get("text");
-  const quoted =
-    input &&
-    typeof input === "string" &&
-    input
-      .split(/\n\n/g)
-      .map((line) => `> ${line}`)
-      .join("\n\n");
+    const input = command.get("text");
+    const quoted =
+      input &&
+      typeof input === "string" &&
+      input
+        .split(/\n\n/g)
+        .map((line) => `> ${line}`)
+        .join("\n\n");
 
-  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
-  const completion = await openai.chat.completions.create({
-    model: "gpt-4-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "You are a helpful assistant.",
-      },
-      {
-        role: "user",
-        content: input + "",
-      },
-    ],
-  });
-  const message = completion.choices[0].message.content;
+    const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
+        {
+          role: "user",
+          content: input + "",
+        },
+      ],
+    });
+    const message = completion.choices[0].message.content;
 
-  return c.json({
-    response_type: "in_channel",
-    text: [
-      quoted,
-      `> from @${command.get("user_name")}`,
-      "", //　引用を終えるための空行
-      message,
-    ].join("\n"),
-  });
-});
+    return c.json({
+      response_type: "in_channel",
+      text: [
+        quoted,
+        `> from @${command.get("user_name")}`,
+        "", //　引用を終えるための空行
+        message,
+      ].join("\n"),
+    });
+  })
+  .post("/bot", (c) => {});
 
 export default app;
 
